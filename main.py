93d0030907afaf622c98c6b7cd1672b83aa5e976
Geusen <89836078+Geusen@users.noreply.github.com>
2022-08-20 23:53:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+import requests
+import time
+from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
+# Chromeヘッドレスモード起動
+options = webdriver.ChromeOptions()
+options.headless = True
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+driver = webdriver.Chrome('chromedriver',options=options)
+driver.implicitly_wait(10)
+
+# サイトURL取得
+driver.get("https://www.sbisec.co.jp/ETGate/?_ControlID=WPLETmgR001Control&_PageID=WPLETmgR001Mdtl20&_DataStoreID=DSWPLETmgR001Control&_ActionID=DefaultAID&burl=iris_indexDetail&cat1=market&cat2=index&dir=tl1-idxdtl%7Ctl2-.N225%7Ctl5-jpn&file=index.html&getFlg=on")
+WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
+  
+# ウインドウ幅・高さ指定
+windowSizeWidth = 800
+windowSizeHeight = 600
+windowWidth = windowSizeWidth if windowSizeWidth else driver.execute_script('return document.body.scrollWidth;')
+windowHeight = windowSizeHeight if windowSizeHeight else driver.execute_script('return document.body.scrollHeight;')
+driver.set_window_size(windowWidth, windowHeight)
+
+# 処理後一時待機,スクリーンショット格納,ブラウザ稼働終了
+time.sleep(2)
+driver.save_screenshot('image.png')
+time.sleep(1)
+driver.quit()
