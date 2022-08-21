23d5f5c40cc0f002bc3cd665799068e1c46d3314
Geusen <89836078+Geusen@users.noreply.github.com>
2022-08-21 15:11:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -13,58 +13,54 @@ from selenium.webdriver.support import expected_conditions as EC
 
 #--------------------------------------------------------------------------------------------------------------------------------------
 webhook_url = os.environ.get("WEBHOOK")
-a = {'任天堂':{'yahoo':'7974.T', 'google':'7974:TYO', 'currency':'円'}
-abc = "7974:TYO"
+l = {'任天堂':{'yahoo':'7974.T', 'google':'7974:TYO', 'currency':'円'}, 'SONY':{'yahoo':'6758.T', 'google':'6758:TYO', 'currency':'円'}}
+def format(cl):
+  rs = soup.find(class_=cl)
+  rs = [i.strip() for i in rs.text.splitlines()]
+  rs = [i for i in rs if i != ""]
+  return rs
 #--------------------------------------------------------------------------------------------------------------------------------------
-#株価取得
-url = "https://finance.yahoo.co.jp/quote/7974.T"
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'html.parser')
-rs = soup.find(class_='_3rXWJKZF')
-rs = [i.strip() for i in rs.text.splitlines()]
-rs = [i for i in rs if i != ""]
-today = rs[0]
-rs = soup.find(class_='_1-yujUee Y_utZE_b')
-rs = [i.strip() for i in rs.text.splitlines()]
-rs = [i for i in rs if i != ""]
-ratio = rs[0]
-
-#--------------------------------------------------------------------------------------------------------------------------------------
-# Chromeヘッドレスモード起動
-options = webdriver.ChromeOptions()
-options.headless = True
-options.add_argument('--no-sandbox')
-options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome('chromedriver',options=options)
-driver.implicitly_wait(10)
-
-# サイトURL取得
-driver.get("https://www.google.com/finance/quote/" + abc + "?hl=ja&gl=JP&window=1M")
-WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
+for x, y in l.items():
+  if y['currency'] = '円':
+    # 株価取得
+    url = "https://finance.yahoo.co.jp/quote/7974.T"
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    format('_3rXWJKZF')
+    today = rs[0]
+    format('_1-yujUee Y_utZE_b')
+    ratio = rs[0]
   
-# ウインドウ幅・高さ指定
-windowSizeWidth = 800
-windowSizeHeight = 600
-#windowWidth = windowSizeWidth if windowSizeWidth else driver.execute_script('return document.body.scrollWidth;')
-#windowHeight = windowSizeHeight if windowSizeHeight else driver.execute_script('return document.body.scrollHeight;')
-driver.set_window_size(windowSizeWidth, windowSizeHeight)
+#--------------------------------------------------------------------------------------------------------------------------------------
+  # Chromeヘッドレスモード起動
+  options = webdriver.ChromeOptions()
+  options.headless = True
+  options.add_argument('--no-sandbox')
+  options.add_argument('--disable-dev-shm-usage')
+  driver = webdriver.Chrome('chromedriver',options=options)
+  driver.implicitly_wait(10)
 
-# 処理後一時待機,スクリーンショット格納,ブラウザ稼働終了
-time.sleep(2)
-driver.save_screenshot('before.png')
-time.sleep(1)
-driver.quit()
+  # サイトURL取得,ウインドウ幅・高さ指定
+  driver.get("https://www.google.com/finance/quote/" + y['google'] + "?hl=ja&gl=JP&window=1M")
+  WebDriverWait(driver, 15).until(EC.presence_of_all_elements_located)
+  driver.set_window_size(800, 600)
 
-# 画像トリミング
-im = Image.open('before.png')
-im.crop((0, 115, 770, 550)).save('image.png', quality=95)
+  # 処理後一時待機,スクリーンショット格納,ブラウザ稼働終了
+  time.sleep(2)
+  driver.save_screenshot('before.png')
+  time.sleep(1)
+  driver.quit()
+  
+  # 画像トリミング
+  im = Image.open('before.png')
+  im.crop((0, 115, 770, 550)).save('image.png', quality=95)
 
 #--------------------------------------------------------------------------------------------------------------------------------------
-text = '今日の' + 'test' + 'の株価は' + str(today) + 'ドル' + 'で、前日比は' + str(ratio) + 'でした。'
-content = {'content': text}
-headers = {'Content-Type': 'application/json'}
-with open('image.png', 'rb') as f:
-    file_bin = f.read()
-image = {'upload' : ('image.png', file_bin)}
-response = requests.post(webhook_url, json.dumps(content), headers=headers)
-response = requests.post(webhook_url, files = image)
+  text = '今日の' + 'test' + 'の株価は' + str(today) + y['currency'] + 'で、前日比は' + str(ratio) + 'でした。'
+  content = {'content': text}
+  headers = {'Content-Type': 'application/json'}
+  with open('image.png', 'rb') as f:
+      file_bin = f.read()
+  image = {'upload' : ('image.png', file_bin)}
+  response = requests.post(webhook_url, json.dumps(content), headers=headers)
+  response = requests.post(webhook_url, files = image)
